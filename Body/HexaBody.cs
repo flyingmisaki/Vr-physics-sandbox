b0acc51d65cddb007dc0f1690728986159a44909
pierr <pierrick@live.co.uk>
2022-06-01 20:28:39 +0100

    Roomscale with movement workaround, its good enough

--- Body/HexaBody.cs
+++ Body/HexaBody.cs
@@ -12,7 +12,6 @@ public class HexaBody : MonoBehaviour {
     public XRRig XRRig;
     public GameObject XRCamera;
     public GameObject CameraOffset;
-    public GameObject OffsetBody;
 
     [Header("Controllers")]
     public ActionBasedController CameraController;
@@ -59,10 +58,6 @@ public class HexaBody : MonoBehaviour {
     bool moving = false;
 
     // Input
-    private Quaternion headYaw;
-    private Vector3 moveDirection;
-    private Vector3 sphereTorque;
-
     private Vector3 cameraControllerPosition;
     private Vector3 rightHandControllerPosition;
     private Vector3 leftHandControllerPosition;
@@ -80,12 +75,16 @@ public class HexaBody : MonoBehaviour {
     private float RightTrackpadTouched;
     private float LeftTrackpadTouched;
 
+    private Quaternion headYaw;
+    private Vector3 moveDirection;
+    private Vector3 sphereTorque;
     private Vector3 bodyOffset;
 
     void Start() {
         additionalHeight = (0.5f * Sphere.transform.lossyScale.y) + (0.5f * Fender.transform.lossyScale.y) + (Head.transform.position.y - Chest.transform.position.y);
     }
 
+    // On every physics update
     private void FixedUpdate() {
         GetControllerInputs();
         CalculateValues();
@@ -94,13 +93,13 @@ public class HexaBody : MonoBehaviour {
         RigToBody();
         Jump();
         if (!jumping) PhysicalCrouch();
-        Debugs();
+        // Debugs();
     }
 
     private void Debugs() {
         Debug.Log("Jumping: "+ jumping);
         Debug.Log("Moving: "+ moving);
-        Debug.Log("BODY OFFSET: "+ bodyOffset.x);
+        Debug.Log("BODY OFFSET: "+ bodyOffset);
     }
 
     // Gets controller inputs
@@ -132,14 +131,19 @@ public class HexaBody : MonoBehaviour {
         headYaw = Quaternion.Euler(0, XRCamera.transform.eulerAngles.y, 0);
         moveDirection = headYaw * new Vector3(LeftTrackpad.x, 0, LeftTrackpad.y);
         sphereTorque = new Vector3(moveDirection.z, 0, -moveDirection.x);
-        bodyOffset = Sphere.transform.position - XRRig.transform.position;
+        // bodyOffset.x = Body.transform.position.x - XRRig.transform.position.x;
+        // bodyOffset.z = Body.transform.position.z - XRRig.transform.position.z;
     }
 
     // Camera and Rig stuff
     private void RigToBody() {
         // Roomscale
-        Head.transform.position = new Vector3(CameraController.transform.position.x, Head.transform.position.y, CameraController.transform.position.z);
+        Body.transform.position = new Vector3(CameraController.transform.position.x, Body.transform.position.y, CameraController.transform.position.z);
         XRCamera.transform.rotation = CameraController.transform.rotation;
+
+        // Body.transform.position = cameraControllerPosition;
+        // XRCamera.transform.position = Head.transform.position;
+        // XRRig.transform.position = new Vector3(Fender.transform.position.x, Fender.transform.position.y - (0.5f * Fender.transform.localScale.y + 0.5f * Sphere.transform.localScale.y), Fender.transform.position.z);
     }
 
     // Movement
@@ -152,7 +156,9 @@ public class HexaBody : MonoBehaviour {
     private void RotateBody() {
         if (RightTrackpadPressed == 1) return;
         Head.transform.Rotate(0, RightTrackpad.x * turnSpeed, 0, Space.Self);
-        XRRig.transform.RotateAround(Head.transform.position, Vector3.up, RightTrackpad.x * turnSpeed);
+        // Body.transform.Rotate(0, RightTrackpad.x * turnSpeed, 0, Space.Self);
+        // PlayerController.transform.Rotate(0, RightTrackpad.x * turnSpeed, 0, Space.Self);
+        XRRig.transform.RotateAround(Body.transform.position, Vector3.up, RightTrackpad.x * turnSpeed);
         Chest.transform.rotation = headYaw;
     }
     
