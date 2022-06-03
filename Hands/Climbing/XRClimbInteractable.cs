9e65ee907bf2fae96869b1abdf0aeb35e999dd25
pierr <pierrick@live.co.uk>
2022-06-03 18:09:12 +0100

    Started work on climbing feature

--- Hands/Climbing/XRClimbInteractable.cs
+++ Hands/Climbing/XRClimbInteractable.cs
@@ -6,9 +6,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class XRClimbInteractable : XRBaseInteractable {
     protected override void OnSelectEntered(XRBaseInteractor interactor) {
         base.OnSelectEntering(interactor);
+        Climber.climbingHand = interactor.GetComponent<XRController>();
     }
 
     protected override void OnSelectExited(XRBaseInteractor interactor) {
         base.OnSelectExiting(interactor);
+        if (Climber.climbingHand && Climber.climbingHand.name == interactor.name) Climber.climbingHand = null;
     }
 }
